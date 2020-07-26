561496921fbff03befaa37964411ba4852a8190a
Andrey Korolyov <berillium11@gmail.com>
2020-07-26 17:12:47 +0300

    Updates

---
+++ precache-manifest.af9b14a630d12d77c2caa98761017a82.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb799c466ad893add22ebb318d1ed75b",
+    "url": "/beryllium11/social_react_project.git/index.html"
+  },
+  {
+    "revision": "d42141c39d932e71ef06",
+    "url": "/beryllium11/social_react_project.git/static/css/main.4fd2601d.chunk.css"
+  },
+  {
+    "revision": "4a08ddcdda604a0bc4fe",
+    "url": "/beryllium11/social_react_project.git/static/js/2.e77af8ff.chunk.js"
+  },
+  {
+    "revision": "bb8fb1c4ca51bfe3d3f76c8caf6f2c92",
+    "url": "/beryllium11/social_react_project.git/static/js/2.e77af8ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d42141c39d932e71ef06",
+    "url": "/beryllium11/social_react_project.git/static/js/main.70fe6dbf.chunk.js"
+  },
+  {
+    "revision": "9615ef380c012ff735e1",
+    "url": "/beryllium11/social_react_project.git/static/js/runtime-main.75a8d51b.js"
+  },
+  {
+    "revision": "e11b93a023968f8991061d0ba4714f2f",
+    "url": "/beryllium11/social_react_project.git/static/media/def-ava.e11b93a0.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/beryllium11/social_react_project.git/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "614321fe76f8df1727ad79f147d08075",
+    "url": "/beryllium11/social_react_project.git/static/media/my-ava.614321fe.png"
+  }
+]);
